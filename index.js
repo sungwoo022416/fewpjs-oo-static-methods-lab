2e6e1f05f8c194562c673466947632efc1a0b687
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-07-25 12:14:00 -0700

    commit tree e89bdcde66c0c18d9eb2eaba99c194ea8e58b197

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string){
+    let execept = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let result = [];
+    
+    let wordArray = string.split( " " );
+    for (let i = 0; i < wordArray.length; i++){
+      if(i == 0){
+        result.push(this.capitalize(wordArray[i]))
+      }
+      else{
+        if(execept.includes(wordArray[i])) {
+          result.push(wordArray[i])
+        }
+        else {
+        result.push(this.capitalize(wordArray[i]))
+        }
+      }
+            
+    } 
+    return result.join( " ");
+  }
 }
